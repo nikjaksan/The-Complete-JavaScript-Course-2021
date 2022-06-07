936012b89d86bb1205f410e38847042822be2c3e
Nicholas Santiago <nickjakesantiago@gmail.com>
2022-06-07 11:36:51 +0200

    updated content

--- 005 Dev Skills & Editor Setup/script.js
+++ 005 Dev Skills & Editor Setup/script.js
@@ -1,7 +1,35 @@
 'use strict';
 
-console.log('Hello World.');
-const x = 23;
-if (x === 23) console.log(23);
+const temperatures1 = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const temperatures2 = [8, 42, 13, -16, 7, 5, -1, 18, 33, -16, 'error', 17]
+// determine max value of array
+/*
+const calcTempAmplitude = function (temps) {
+    let max = temps[0], min = temps[0];
 
-const calcAge = birthYear => 2022 - birthYear;
+    for (let i = 0; i < temperatures.length; i++) {
+        const currTemp = temps[i];
+
+        if (currTemp < min) min = currTemp;
+        if (currTemp > max) max = currTemp;
+    }
+    return max - min;
+}
+
+console.log('The temperature range is ' + calcTempAmplitude(temperatures));
+*/
+
+const calcTempAmplitudeNew = function (temp1, temp2) {
+    const tempMerge = temp1.concat(temp2);
+    // concatenate temp2 on temp1 and store it as a new array called tempMerge
+    let max = tempMerge[0], min = tempMerge[0];
+
+    for (let i = 0; i < tempMerge.length; i++) {
+        const currTemp = tempMerge[i];
+
+        if (currTemp < min) min = currTemp;
+        if (currTemp > max) max = currTemp;
+    }
+    return max - min;
+}
+console.log('The temperature range is ' + calcTempAmplitudeNew(temperatures1, temperatures2));
