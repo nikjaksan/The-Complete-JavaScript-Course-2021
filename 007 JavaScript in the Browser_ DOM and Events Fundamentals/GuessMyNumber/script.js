ef6d83117258a282e804352104888b0d33f2337d
Nicholas Santiago <nickjakesantiago@gmail.com>
2022-06-08 22:54:06 +0200

    updated content

--- 007 JavaScript in the Browser_ DOM and Events Fundamentals/GuessMyNumber/script.js
+++ 007 JavaScript in the Browser_ DOM and Events Fundamentals/GuessMyNumber/script.js
@@ -7,7 +7,7 @@ console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.number').textContent = 13; //change '?'(HTML) >> '13' (DOM Manip.)
 document.querySelector('.score').textContent = 10; //change '20'(HTML) >> '10' (DOM Manip.)
-*/
+
 
 // 073 Handling Click Events
 // using function declaration
@@ -58,3 +58,47 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'You MUST choose a number between 1 and 20!';
   }
 });
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(typeof secretNumber, secretNumber);
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+
+
+console.log(typeof document.querySelector('.guess').value);
+
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ Not a valid number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = `${secretNumber} is the correct answer! 🎉`
+  } else if (guess > secretNumber && guess < 21) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Choose a smaller number';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🚯 You lose';
+      document.querySelector('.score').textContent = '0';
+    }
+  } else if (guess < secretNumber && guess > 0) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Choose a larger number';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🚯 You lose';
+      document.querySelector('.score').textContent = '0';
+    }
+  } else {
+    document.querySelector('.message').textContent = 'Choose a number between 1 & 20'
+  }
+})
+if (document.querySelector(''))
