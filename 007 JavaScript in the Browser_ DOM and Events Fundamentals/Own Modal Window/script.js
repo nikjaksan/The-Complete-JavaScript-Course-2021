89015c24e230cb6b4e79fecb03b9d05ff4038863
Nicholas Santiago <nickjakesantiago@gmail.com>
2022-06-17 10:20:12 +0200

    updated content

---
+++ 007 JavaScript in the Browser_ DOM and Events Fundamentals/Own Modal Window/script.js
@@ -0,0 +1,6 @@
+'use strict';
+// document.querySelector('cats-show-modal').addEventListener('click', function () {
+//     document.querySelector('catsModal').remove('hidden');
+//})
+
+console.log('Hello World')
